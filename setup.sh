7dc3388874501c7d123b760bfec02ad5387ac7af
Nil-Montia <49072070+Nil-Montia@users.noreply.github.com>
2019-06-03 14:39:37 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+export GMAIL_USER="myemail.noreply@gmail.com"
+export GMAIL_PASS="password"
+export SERVICE_NAME="Example Project"
+export ACTIVATION_LINK="http://localhost/authentication/api/activate"
+docker-compose up -d
