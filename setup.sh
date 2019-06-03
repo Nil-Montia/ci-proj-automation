fa509c63852c0bb8473aff34741fd3c4e9bbb945
Nil-Montia <49072070+Nil-Montia@users.noreply.github.com>
2019-06-03 16:11:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export GMAIL_USER="ilovedocker123@gmail.com"
-export GMAIL_PASS="ilovedocker1!"
+export GMAIL_USER=ilovedocker123@gmail.com
+export GMAIL_PASS=ilovedocker1!
 export SERVICE_NAME="Example Project"
-export ACTIVATION_LINK="http://35.242.243.64/authentication/api/activate"
+export ACTIVATION_LINK=http://35.242.243.64/authentication/api/activate
